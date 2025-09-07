690aa691a13dd05d87021801fbc894fa207dc198
KarlaXBT <kaarel.leib@protonmail.com>
2025-09-07 19:43:13 +0300

    Kasutasin useRef hooki

--- tegelased/src/pages/LisaTegelane.jsx
+++ tegelased/src/pages/LisaTegelane.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function LisaTegelane() {
   const [message, setMessage] = useState("");
+  const nimiRef = useRef(null);
+
+  function lisaUusTegelane() {
+    if (nimiRef.current.value.trim() === "") {
+      setMessage("Tühja nimega ei saa sisestada");
+    } else {
+      setMessage("Tegelane lisatud");
+    }
+    nimiRef.current.value = "";
+  }
   return (
     <div>
       <div>{message}</div>
       <label htmlFor="tegelane">Tegelase Nimi</label>
-      <input id="tegelane" type="text" />
-      <button onClick={() => setMessage("Tegelane lisatud!")}>Lisa uus</button>
+      <input id="tegelane" type="text" ref={nimiRef} />
+      <button onClick={() => lisaUusTegelane()}>Lisa uus</button>
     </div>
   );
 }
