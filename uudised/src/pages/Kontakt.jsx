1808b39c52d7c2b965b8feb9b6fb442b911b57bd
KarlaXBT <kaarel.leib@protonmail.com>
2025-09-07 18:03:06 +0300

    Muutsin useState kasutades meist ning kontakti komponente erinevat infot kuvama

--- uudised/src/pages/Kontakt.jsx
+++ uudised/src/pages/Kontakt.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Kontakt() {
+  const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState("");
+  const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState("");
+  const [n2itaTelTasku, muudaN2itaTelTasku] = useState("");
   return (
+    // <div>
+    //   <div>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</div>
+    //   <div>Võtke meiega ühendust:</div>
+    //   <br />
+    //   <div>Kristiine keskus</div>
+    //   <div>+372683694</div>
+    //   <div>Endla 45</div>
+    //   <br />
+    //   <div>Kristiine keskus</div>
+    //   <div>+372683694</div>
+    //   <div>Endla 45</div>
+    //   <br />
+    //   <div>Kristiine keskus</div>
+    //   <div>+372683694</div>
+    //   <div>Endla 45</div>
+    // </div>
+
     <div>
       <div>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</div>
       <div>Võtke meiega ühendust:</div>
       <br />
-      <div>Kristiine keskus</div>
-      <div>+372683694</div>
+      <div onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>
+        Kristiine keskus
+      </div>
+      {n2itaTelKristiine && <div>+372683694</div>}
       <div>Endla 45</div>
       <br />
-      <div>Kristiine keskus</div>
-      <div>+372683694</div>
-      <div>Endla 45</div>
+      <div onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>
+        Ülemiste keskus
+      </div>
+      {n2itaTelYlemiste && <div>+372683694</div>}
+      <div>Suur-Sõjamäe 4</div>
       <br />
-      <div>Kristiine keskus</div>
-      <div>+372683694</div>
-      <div>Endla 45</div>
+      <div onClick={() => muudaN2itaTelTasku(!n2itaTelTasku)}>Tasku Keskus</div>
+      {n2itaTelTasku && <div>+372683694</div>}
+      <div>Turu 2</div>
     </div>
   );
 }
--- uudised/src/pages/Meist.jsx
+++ uudised/src/pages/Meist.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Meist() {
+  const [kontakt, n2itaKontakt] = useState("");
   return (
     <div>
       <div>See on meist leht, nähtav localhost:3000/meist aadressil</div>
       <div>Meie töötajad:</div>
       <br />
       <div>Arvo Pärt</div>
-      <div>+3726409437</div>
+
       <div>Uudisklippide taustamuusika</div>
+      <button onClick={() => n2itaKontakt("+37264094372")}>
+        Võta ühenduse
+      </button>
       <br />
       <div>Kelly Sildaru</div>
-      <div>+3726409437</div>
+
       <div>Püstolreporter</div>
+      <button onClick={() => n2itaKontakt("+3726409437")}>Võta ühenduse</button>
       <br />
       <div>Edward von Lõngus</div>
-      <div>+3726409437</div>
+
       <div>Uudiste kujundamine</div>
+      <button onClick={() => n2itaKontakt("+3726423437")}>Võta ühenduse</button>
       <br />
       <div>Kerli Kõiv</div>
-      <div>+3726409437</div>
+
       <div>Välistugude spetsialist</div>
+      <button onClick={() => n2itaKontakt("+3726434237")}>Võta ühenduse</button>
+      <br />
+      <br />
+      {kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}
     </div>
   );
 }
