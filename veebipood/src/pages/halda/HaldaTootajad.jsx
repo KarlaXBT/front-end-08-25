bcadb2c4ea8efbec2eadbafc5ac25f4017d82ca7
KarlaXBT <kaarel.leib@protonmail.com>
2025-09-19 08:26:31 +0300

    kirjutasin töötajatele ja toodetele andmete dünaamiliselt kuvamise, kustamise,lisamise, dünaamilised klassid

--- veebipood/src/pages/halda/HaldaTootajad.jsx
+++ veebipood/src/pages/halda/HaldaTootajad.jsx
@@ -1,5 +1,156 @@
+import { useState } from "react";
+
+// import .json failist
+import tootajadFailist from "../../data/tootajad.json";
+
 function HaldaTootajad() {
-  return <div>HaldaTootajad</div>;
+  const [tootajad, setTootajad] = useState(tootajadFailist);
+
+  const [uusTootaja, setUusTootaja] = useState({
+    id: "",
+    eesnimi: "",
+    perenimi: "",
+    amet: "",
+    osakond: "",
+    palk: "",
+    tootav: false,
+  });
+
+  // sellega saame objektide võtmed returnib array
+  const tootajadVotmed = Object.keys(tootajadFailist[0]);
+
+  function kustuta(i) {
+    // koopia, et referentsi uuendada
+    const uusTootajad = [...tootajad];
+    uusTootajad.splice(i, 1);
+    setTootajad(uusTootajad);
+  }
+
+  function lisa() {
+    const tootajadKoopia = [...tootajad];
+    tootajadKoopia.push(uusTootaja);
+    setTootajad(tootajadKoopia);
+    setUusTootaja({
+      id: "",
+      eesnimi: "",
+      perenimi: "",
+      amet: "",
+      osakond: "",
+      palk: "",
+      tootav: false,
+    });
+  }
+  return (
+    <>
+      <h1>Meie töötajad</h1>
+      {/* tabel et state muutujast kuvada */}
+
+      <table>
+        <thead>
+          <tr>
+            {/* .map et need dünaamiliselt kuvada */}
+            {tootajadVotmed.map((item, index) => (
+              <th key={index}>{item}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {/* .map jälle et state muutujast kuvada tabelisse andmed */}
+          {tootajad.map((item, index) => (
+            <tr className={item.tootav ? "tootav" : "mitte-tootav"} key={index}>
+              <td>{item.id}</td>
+              <td>{item.eesnimi}</td>
+              <td>{item.perenimi}</td>
+              <td>{item.amet}</td>
+              <td>{item.osakond}</td>
+              <td>{item.palk}</td>
+              <td>
+                {/* saame muuta booleani */}
+                <input
+                  onChange={() => {
+                    const koopia = [...tootajad];
+                    koopia[index].tootav = !koopia[index].tootav;
+                    setTootajad(koopia);
+                  }}
+                  checked={item.tootav}
+                  type="checkbox"
+                />
+              </td>
+              <td>
+                <button onClick={() => kustuta(index)}> X</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Lisa töötaja</h2>
+      <label>Id</label>
+      <br />
+      <input
+        onChange={(e) => setUusTootaja({ ...uusTootaja, id: e.target.value })}
+        value={uusTootaja.id}
+        type="number"
+      />
+      <br />
+      <label>Eesnimi</label>
+      <br />
+      <input
+        onChange={(e) =>
+          setUusTootaja({ ...uusTootaja, eesnimi: e.target.value })
+        }
+        value={uusTootaja.eesnimi}
+        type="text"
+      />
+      <br />
+      <label>Perenimi</label>
+      <br />
+      <input
+        onChange={(e) =>
+          setUusTootaja({ ...uusTootaja, perenimi: e.target.value })
+        }
+        value={uusTootaja.perenimi}
+        type="text"
+      />
+      <br />
+      <label>Amet</label>
+      <br />
+      <input
+        onChange={(e) => setUusTootaja({ ...uusTootaja, amet: e.target.value })}
+        value={uusTootaja.amet}
+        type="text"
+      />
+      <br />
+      <label>Osakond</label>
+      <br />
+      <input
+        onChange={(e) =>
+          setUusTootaja({ ...uusTootaja, osakond: e.target.value })
+        }
+        value={uusTootaja.osakond}
+        type="text"
+      />
+      <br />
+      <label>Palk</label>
+      <br />
+      <input
+        onChange={(e) => setUusTootaja({ ...uusTootaja, palk: e.target.value })}
+        value={uusTootaja.palk}
+        type="number"
+      />
+      <br />
+      <label>Töötav</label>
+      <br />
+      <input
+        onChange={(e) =>
+          setUusTootaja({ ...uusTootaja, tootav: e.target.checked })
+        }
+        checked={uusTootaja.tootav}
+        type="checkbox"
+      />
+      <br />
+      <button onClick={lisa}>Lisa</button>
+    </>
+  );
 }
 
 export default HaldaTootajad;
--- veebipood/src/pages/halda/HaldaTooted.jsx
+++ veebipood/src/pages/halda/HaldaTooted.jsx
@@ -1,5 +1,95 @@
+import { useState } from "react";
+import tootedFailist from "../../data/tooted.json";
+
 function HaldaTooted() {
-  return <div>HaldaTooted</div>;
+  const [tooted, setTooted] = useState(tootedFailist);
+
+  const [uusToode, setUusToode] = useState({
+    nimi: "",
+    kogus: "",
+    hind: "",
+    ostetud: false,
+  });
+
+  function kustuta(i) {
+    const uuendatudTooted = [...tooted];
+    uuendatudTooted.splice(i, 1);
+    setTooted(uuendatudTooted);
+  }
+
+  function lisa() {
+    const uuendatudTooted = [...tooted];
+    uuendatudTooted.push({
+      ...uusToode,
+      kogus: Number(uusToode.kogus),
+      hind: Number(uusToode.hind),
+    });
+    setTooted(uuendatudTooted);
+    setUusToode({ nimi: "", kogus: "", hind: "" });
+  }
+  return (
+    <>
+      <h2>Tooted</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nimi</th>
+            <th>Kogus</th>
+            <th>Hind</th>
+            <th>Ostukorvis</th>
+            <th>Kustuta</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tooted.map((item, index) => (
+            <tr key={index}>
+              <td> {item.nimi} </td>
+              <td> {item.kogus} </td>
+              <td> {item.hind} </td>
+
+              <td> {item.ostetud ? "Ostetud" : "Ostmata"} </td>
+              <td>
+                <button onClick={() => kustuta(index)}>X</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>Lisa uus toode</h2>
+      <label htmlFor=""> Nimi</label>
+      <input
+        onChange={(e) => setUusToode({ ...uusToode, nimi: e.target.value })}
+        value={uusToode.nimi}
+        type="text"
+      />
+      <br />
+      <label htmlFor=""> Kogus</label>
+      <input
+        onChange={(e) => setUusToode({ ...uusToode, kogus: e.target.value })}
+        value={uusToode.kogus}
+        type="number"
+      />
+      <br />
+      <label htmlFor=""> Hind</label>
+      <input
+        onChange={(e) => setUusToode({ ...uusToode, hind: e.target.value })}
+        value={uusToode.hind}
+        type="number"
+      />
+      <br />
+      <label htmlFor=""> Ostetud</label>
+      <input
+        onChange={(e) =>
+          setUusToode({ ...uusToode, ostetud: e.target.checked })
+        }
+        checked={uusToode.ostetud}
+        type="checkbox"
+      />
+      <br />
+      <button onClick={lisa}>Lisa toode</button>
+    </>
+  );
 }
 
 export default HaldaTooted;
