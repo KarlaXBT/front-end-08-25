c64c2a987daf7dace4b77a11f3f841a9f6b5492b
KarlaXBT <kaarel.leib@protonmail.com>
2025-09-25 10:06:02 +0300

    tegin muudatusi

--- tagasiside/src/pages/Tagasiside.jsx
+++ tagasiside/src/pages/Tagasiside.jsx
@@ -7,11 +7,46 @@ function Tagasiside() {
     "Teistsugune",
     "Põnev",
   ]);
+
+  const [lisaUus, setLisaUus] = useState("");
+
+  function kustuta(index) {
+    const koopia = [...tagasisided];
+    koopia.splice(index, 1);
+    setTagasisided(koopia);
+  }
+
+  function lisa() {
+    if (!lisaUus.trim()) {
+      alert("Tühjust ei saa lisada");
+      return;
+    }
+
+    const koopia = [...tagasisided];
+    koopia.push(lisaUus);
+    setTagasisided(koopia);
+    setLisaUus("");
+  }
+
   return (
     <div>
-      {tagasisided.map((i) => (
-        <div>{i}</div>
+      {tagasisided.map((i, index) => (
+        <div key={index}>
+          {i}
+          <button onClick={() => kustuta(index)}>X</button>
+        </div>
       ))}
+
+      <label>Lisa uus tagasiside: </label>
+      <input
+        onChange={(e) => setLisaUus(e.target.value)}
+        value={lisaUus}
+        type="text"
+      />
+      <button onClick={lisa}>Sisesta</button>
+      <br />
+      {JSON.stringify(lisaUus)}
+      {JSON.stringify(tagasisided)}
     </div>
   );
 }
